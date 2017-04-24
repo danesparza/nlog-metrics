b8c8ac7d63f4757f268595891dc01a3591c3a61d
Dan Esparza <esparza.dan@gmail.com>
2017-04-24 13:34:50 -0400

    Updated .bat file formatting
    Removed batch files for now

--- dist/uninstall.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-:: This needs to be run with an Administrator command prompt
-:: See https://technet.microsoft.com/en-us/library/cc947813(v=ws.10).aspx for more information
-
-net stop nlog-metrics
-nssm remove nlog-metrics
