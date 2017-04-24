a033309ea2ea28c74fbb671b12984fa4497f41f5
Dan Esparza <esparza.dan@gmail.com>
2017-04-24 13:49:48 -0400

    Fix NSSM install batch files
    Fix for #1 - added line for nssm install and updated batch file
    encoding.

--- dist/install.bat
+++ dist/install.bat
@@ -4,7 +4,7 @@
 
 :: Update the path to match your machine:
 nssm install nlog-metrics d:\services\nlog-metrics\nlog-metrics_windows_amd64.exe
-
+nssm set nlog-metrics AppParameters start
 nssm set nlog-metrics Description NLog metrics collection and logging
 nssm set nlog-metrics Start SERVICE_AUTO_START
 
